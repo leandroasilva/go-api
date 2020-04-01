8443eaf3ff48c6ba6ad174dcc61adaa0b2fe1393
leandroasilva <halk_lenson@hotmail.com>
2020-04-01 15:04:25 -0300

    add model structure

--- model/usuarios.go
+++ model/usuarios.go
@@ -1,1 +1,8 @@
 package model
+
+// Usuarios representa a tabela de usuarios na base de dados.
+type Usuarios struct {
+	ID    int    `db:"id" json:"id"`
+	Nome  string `db:"nome" json:"nome"`
+	Email string `db:"email" json:"email"`
+}
